06ee375488a917fe3953727bc9b54ed36a1164a8
David Whiting <david.g.whiting@gmail.com>
2018-05-02 16:28:36 -0500

    Conditioning_Stability.py

---
+++ Volume1/python/Conditioning_Stability.py
@@ -0,0 +1,99 @@
+import numpy as np
+from scipy import linalg as la
+
+A = np.array([[1, 1], [1, 1+1e-10]])
+np.linalg.cond(A)
+
+# Set up and solve a simple system of equations.
+b1 = np.array([2, 2])
+x1 = la.solve(A, b1)
+print(x1)
+
+# Solve a system with a very slightly different vector b.
+b2 = np.array([2, 2+1e-5])
+la.norm(b1 - b2)
+x2 = la.solve(A, b2)
+print(x2)
+
+
+w_coeffs = np.array([1, -210, 20615, -1256850, 53327946, -1672280820,
+                    40171771630, -756111184500, 11310276995381,
+                    -135585182899530, 1307535010540395,
+                    -10142299865511450, 63030812099294896,
+                    -311333643161390640, 1206647803780373360,
+                    -3599979517947607200, 8037811822645051776,
+                    -12870931245150988800, 13803759753640704000,
+                    -8752948036761600000, 2432902008176640000])
+w_roots = np.arange(1, 21)
+
+import sympy as sy
+from matplotlib import pyplot as plt
+
+# The roots of w are 1, 2, ..., 20.
+w_roots = np.arange(1, 21)
+
+# Get the exact Wilkinson polynomial coefficients using SymPy.
+x, i = sy.symbols('x i')
+w = sy.poly_from_expr(sy.product(x-i, (i, 1, 20)))[0]
+w_coeffs = np.array(w.all_coeffs())
+print(w_coeffs[:6])
+
+# Perturb one of the coefficients very slightly.
+h = np.zeros(21)
+h[1]=1e-7
+new_coeffs = w_coeffs - h
+print(new_coeffs[:6])
+
+# Use NumPy to compute the roots of the perturbed polynomial.
+new_roots = np.roots(np.poly1d(new_coeffs))
+
+# Sort the roots to ensure that they are in the same order.
+w_roots = np.sort(w_roots)
+new_roots = np.sort(new_roots)
+
+# Estimate the absolute condition number in the infinity norm.
+k = la.norm(new_roots - w_roots, np.inf) / la.norm(h, np.inf)
+print(k)
+
+# Estimate the relative condition number in the infinity norm.
+k * la.norm(w_coeffs, np.inf) / la.norm(w_roots, np.inf)
+
+reals = np.random.normal(0, 1e-10, A.shape)
+imags = np.random.normal(0, 1e-10, A.shape)
+H = reals + 1j*imags
+
+# w_coeffs holds the coefficients and w_roots holds the true roots.
+computed_roots = np.sort(np.roots(np.poly1d(w_coeffs)))
+print(computed_roots[:6])       # The computed roots are close to integers.
+
+# Compute the forward error.
+forward_error = la.norm(w_roots - computed_roots)
+print(forward_error)
+
+# Compute the relative forward error.
+forward_error / la.norm(w_roots)
+
+# Use least squares to approximate sin(x) with a five-degree polynomial.
+x = np.linspace(0, 6, 10)           # The x-values of the data.
+b = np.sin(x) + .2*np.random.randn(10) # The y-values of the data (noisy).
+A = np.vander(x, 6)                 # Set up the matrix of data values.
+coeffs = la.lstsq(A, b)[0]          # Get the polynomial coefficients.
+
+domain = np.linspace(0, 6, 100)     # Define a finer domain for plotting.
+plt.plot(x, b, 'k*')				# Plot the approximated points.
+plt.plot(domain, np.sin(domain))
+
+# np.poly1d() converts an array of coefficients into a callable polynomial.
+plt.plot(domain, np.poly1d(coeffs)(domain))
+
+xk, yk = np.load("stability_data.npy").T
+A = np.vander(xk, n+1)
+
+from math import sqrt           # np.sqrt() fails for very large numbers.
+
+a = 10**20 + 1
+b = 10**20
+sqrt(a) - sqrt(b)               # Do the subtraction directly.
+
+(a - b) / (sqrt(a) + sqrt(b))   # Use the alternative formulation.
+
