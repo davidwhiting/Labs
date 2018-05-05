a3da7dcf37d2bf0356e681618c35f2aa6e6c64f3
David Whiting <david.g.whiting@gmail.com>
2018-05-04 21:46:42 -0500

    edited 05.py

--- Volume1/python/05-ImageSegmentation.py
+++ Volume1/python/05-ImageSegmentation.py
@@ -1,23 +1,10 @@
 
->>> A = np.array([[0,1,0,0,1,0],[1,0,1,0,1,0],
+A = np.array([[0,1,0,0,1,0],[1,0,1,0,1,0],
                   [0,1,0,1,0,0],[0,0,1,0,1,1],
                   [1,1,0,1,0,0],[0,0,0,1,0,0]])
 
->>> np.linalg.matrix_power(A,2)
-array([[2, 1, 1, 1, 1, 0],
-       [1, 3, 0, 2, 1, 0],
-       [1, 0, 2, 0, 2, 1],
-       [1, 2, 0, 3, 0, 0],
-       [1, 1, 2, 0, 3, 1],
-       [0, 0, 1, 0, 1, 1]])
-
->>> np.linalg.matrix_power(A, 6)
-array([[45, 54, 38, 45, 54, 16],
-       [54, 86, 29, 77, 51, 11],
-       [38, 29, 55, 15, 70, 27],
-       [45, 77, 15, 75, 31,  4],
-       [54, 51, 70, 31, 93, 34],
-       [16, 11, 27,  4, 34, 14]])
+np.linalg.matrix_power(A,2)
+np.linalg.matrix_power(A, 6)
 
 def sparse_generator(n, c):
     """Return a symmetric nxn matrix with sparsity determined by c."""
@@ -25,55 +12,45 @@ def sparse_generator(n, c):
     A = ( A > c**(.5) )
     return A.T @ A
 
->>> from scipy.misc import imread
->>> from matplotlib import pyplot as plt
+from scipy.misc import imread
+from matplotlib import pyplot as plt
 
->>> image = imread("dream.png")     # Read a (very) small image.
->>> print(image.shape)              # Since the array is 3-dimensional,
-(48, 48, 3)                         # this is a color image.
+image = imread("dream.png")     # Read a (very) small image.
+print(image.shape)              # Since the array is 3-dimensional, this is a color image.
 
 # The image is read in as integers from 0 to 255.
->>> print(image.<<min>>(), image.<<max>>(), image.dtype)
-0 254 uint8
+print(image.<<min>>(), image.<<max>>(), image.dtype)
 
 # Scale the image to floats between 0 and 1 for Matplotlib.
->>> scaled = image / 255.
->>> print(scaled.<<min>>(), scaled.<<max>>(), scaled.dtype)
-0.0 0.996078431373 float64
+scaled = image / 255.
+print(scaled.<<min>>(), scaled.<<max>>(), scaled.dtype)
 
 # Display the scaled image.
->>> plt.imshow(scaled)
->>> plt.axis("off")
->>> plt.show()
+plt.imshow(scaled)
+plt.axis("off")
+plt.show()
 
 # Average the RGB values of a colored image to obtain a grayscale image.
->>> brightness = scaled.mean(axis=2)        # Average over the last axis.
->>> print(brightness.shape)                 # Note that the array is now 2-D.
-(48, 48)
+brightness = scaled.mean(axis=2)        # Average over the last axis.
+print(brightness.shape)                 # Note that the array is now 2-D.
 
 # Display the image in gray.
->>> plt.imshow(brightness, cmap="gray")
->>> plt.axis("off")
->>> plt.show()
+plt.imshow(brightness, cmap="gray")
+plt.axis("off")
+plt.show()
 
->>> import numpy as np
->>> A = np.random.randint(0, 10, (3,4))
->>> print(A)
-[[4 4 7 7]
- [8 1 2 0]
- [7 0 0 9]]
+import numpy as np
+A = np.random.randint(0, 10, (3,4))
+print(A)
 
 # Unravel the 2-D array (by rows) into a 1-D array.
->>> np.ravel(A)
-array([4, 4, 7, 7, 8, 1, 2, 0, 7, 0, 0, 9])
+np.ravel(A)
 
 # Unravel a grayscale image into a 1-D array and check its size.
->>> M,N = brightness.shape
->>> flat_brightness = np.ravel(brightness)
->>> M*N == flat_brightness.size
-<<True>>
->>> print(flat_brightness.shape)
-(2304,)
+M,N = brightness.shape
+flat_brightness = np.ravel(brightness)
+M*N == flat_brightness.size
+print(flat_brightness.shape)
 
 def get_neighbors(index, radius, height, width):
     """Calculate the flattened indices of the pixels that are within the given
@@ -107,44 +84,28 @@ def get_neighbors(index, radius, height, width):
     return (X[mask] + Y[mask]*width).astype(np.<<int>>), R[mask]
 
 # Compute the neighbors and corresponding distances from the figure.
->>> neighbors_1, distances_1 = get_neighbors(5, 1.2, 4, 4)
->>> print(neighbors_1, distances_1, sep='\n')
-[1 4 5 6 9]
-[ 1.  1.  0.  1.  1.]
+neighbors_1, distances_1 = get_neighbors(5, 1.2, 4, 4)
+print(neighbors_1, distances_1, sep='\n')
 
 # Increasing the radius from 1.2 to 1.5 results in more neighbors.
->>> neighbors_2, distances_2 = get_neighbors(5, 1.5, 4, 4)
->>> print(neighbors_2, distances_2, sep='\n')
-[ 0  1  2  4  5  6  8  9 10]
-[ 1.41421356  1.          1.41421356  1.          0.          1.
-  1.41421356  1.          1.41421356]
+neighbors_2, distances_2 = get_neighbors(5, 1.5, 4, 4)
+print(neighbors_2, distances_2, sep='\n')
 
->>> x = np.arange(-5,5).reshape((5,2)).T
->>> print(x)
-[[-5 -3 -1  1  3]
- [-4 -2  0  2  4]]
+x = np.arange(-5,5).reshape((5,2)).T
+print(x)
 
 # Construct a boolean mask of x describing which entries of x are positive.
->>> mask = x > 0
->>> print(mask)
-<<[[False False False  True  True]
- [False False False  True  True]]>>
+mask = x > 0
+print(mask)
 
 # Use the mask to zero out all of the nonpositive entries of x.
->>> x * mask
-array([[0, 0, 0, 1, 3],
-       [0, 0, 0, 2, 4]])
+x * mask
 
->>> mask = np.arange(-5,5).reshape((5,2)).T > 0
->>> print(mask)
-<<[[False False False  True  True]
- [False False False  True  True]]>>
+mask = np.arange(-5,5).reshape((5,2)).T > 0
+print(mask)
 
 # The mask can be negated with the tilde operator ~.
->>> print(~mask)
-<<[[ True  True  True False False]
- [ True  True  True False False]]>>
+print(~mask)
 
 # Stack a mask into a 3-D array with np.dstack().
->>> print(mask.shape, np.dstack((mask, mask, mask)).shape)
-(2, 5) (2, 5, 3)
+print(mask.shape, np.dstack((mask, mask, mask)).shape)
