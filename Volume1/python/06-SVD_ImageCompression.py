af9e8717789dca30d8fcbfc51ce5d0dd4fe2241b
David Whiting <david.g.whiting@gmail.com>
2018-05-04 21:48:37 -0500

    edited 06.py

--- Volume1/python/06-SVD_ImageCompression.py
+++ Volume1/python/06-SVD_ImageCompression.py
@@ -1,50 +1,42 @@
 
->>> import numpy as np
->>> from scipy import linalg as la
+import numpy as np
+from scipy import linalg as la
 
 # Generate a random matrix and get its compact SVD via SciPy.
->>> A = np.random.random((10,5))
->>> U,s,Vh = la.svd(A, full_matrices=False)
->>> print(U.shape, s.shape, Vh.shape)
-(10, 5) (5,) (5, 5)
+A = np.random.random((10,5))
+U,s,Vh = la.svd(A, full_matrices=False)
+print(U.shape, s.shape, Vh.shape)
 
 # Verify that U is orthonormal, U Sigma Vh = A, and the rank is correct.
->>> np.allclose(U.T @ U, np.identity(5))
-<<True>>
->>> np.allclose(U @ np.diag(s) @ Vh, A)
-<<True>>
->>> np.linalg.matrix_rank(A) == len(s)
-<<True>>
+np.allclose(U.T @ U, np.identity(5))
+np.allclose(U @ np.diag(s) @ Vh, A)
+np.linalg.matrix_rank(A) == len(s)
 
->>> A = np.random.random((20, 20))
->>> A.size
-400
+A = np.random.random((20, 20))
+A.size
 
->>> from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
 # Send the RGB values to the interval (0,1).
->>> image_gray = plt.imread("hubble_gray.jpg") / 255.
->>> image_gray.shape            # Grayscale images are 2-d arrays.
-(1158, 1041)
+image_gray = plt.imread("hubble_gray.jpg") / 255.
+image_gray.shape            # Grayscale images are 2-d arrays.
 
->>> image_color = plt.imread("hubble.jpg") / 255.
->>> image_color.shape           # Color images are 3-d arrays.
-(1158, 1041, 3)
+image_color = plt.imread("hubble.jpg") / 255.
+image_color.shape           # Color images are 3-d arrays.
 
 # The final axis has 3 layers for red, green, and blue values.
->>> red_layer = image_color[:,:,0]
->>> red_layer.shape
-(1158, 1041)
+red_layer = image_color[:,:,0]
+red_layer.shape
 
 # Display a gray image.
->>> plt.imshow(red_layer, cmap="gray")
->>> plt.axis("off")             # Turn off axis ticks and labels.
->>> plt.show()
+plt.imshow(red_layer, cmap="gray")
+plt.axis("off")             # Turn off axis ticks and labels.
+plt.show()
 
 # Display a color image.
->>> plt.imshow(image_color)     # cmap=None by default.
->>> plt.axis("off")
->>> plt.show()
+plt.imshow(image_color)     # cmap=None by default.
+plt.axis("off")
+plt.show()
 
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
